1311953deafba3845107c743445ff02f7ef0fb7a
MGSimard <mgsimard.dev@gmail.com>
2025-06-10 20:50:39 -0400

    thumbs + aspect ratio (will help with layout shifts later on as well)

--- src/routes/world/index.tsx
+++ src/routes/world/index.tsx
@@ -28,8 +28,13 @@ function PageWorld() {
         <div className="regions-grid">
           <Figure
             caption="Sky Passage, Myth War Online"
-            figureSrc="/assets/game/worldmap_mw1.jpg"
-            fullSrc="/assets/game/worldmap_mw1.jpg"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDMtw4eGnb3uSsWXI26wpKemYEJknqdoAcyfGx"
+            fullSrc=""
+          />
+          <Figure
+            caption="Sky Passage, Myth War II"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDWdO1408Bfw9LNZ4rUvj5JH8gmuxFYyDa7VcE"
+            fullSrc=""
           />
         </div>
         <hr />
@@ -37,8 +42,18 @@ function PageWorld() {
         <div className="regions-grid">
           <Figure
             caption="Woodlingor, Myth War Online"
-            figureSrc="/assets/game/worldmap_mw1.jpg"
-            fullSrc="/assets/game/worldmap_mw1.jpg"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDxrDGMLe6yfmsFp3PlRXvq14WQMGSKYuOVeN2"
+            fullSrc=""
+          />
+          <Figure
+            caption="Woodlingor (Winter), Myth War Online"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2ID4G2AfRKcrwVZDsukISP3qghLp8bQOazGK4Fi"
+            fullSrc=""
+          />
+          <Figure
+            caption="Woodlingor, Myth War II"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDZGmQtlOTc3PzmeJdgr8CjMvVOwFXQISN90Gk"
+            fullSrc=""
           />
         </div>
         <hr />
@@ -46,8 +61,18 @@ function PageWorld() {
         <div className="regions-grid">
           <Figure
             caption="Blython, Myth War Online"
-            figureSrc="/assets/game/worldmap_mw1.jpg"
-            fullSrc="/assets/game/worldmap_mw1.jpg"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDjpfcDIDVouBCRq8DvbQmUzW6wKcrFEkaVS7n"
+            fullSrc=""
+          />
+          <Figure
+            caption="Blython (Winter), Myth War Online"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDWmcVJK8Bfw9LNZ4rUvj5JH8gmuxFYyDa7VcE"
+            fullSrc=""
+          />
+          <Figure
+            caption="Blython, Myth War II"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDODdmg9Wpm7hLnow43kVBXSvNyxGKMAEUZale"
+            fullSrc=""
           />
         </div>
         <hr />
@@ -55,12 +80,29 @@ function PageWorld() {
         <div className="regions-grid">
           <Figure
             caption="Desert City, Myth War Online"
-            figureSrc="/assets/game/worldmap_mw1.jpg"
-            fullSrc="/assets/game/worldmap_mw1.jpg"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDds1q1cGt4PBA5R1Gny2ZwlDKFa7qQpeH6Lor"
+            fullSrc=""
+          />
+          <Figure
+            caption="Desert City, Myth War II"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDY7RnQST4ySH0FN6v9uabPiRkq1XzJLBVfcso"
+            fullSrc=""
           />
         </div>
         <hr />
         <h3>Demon Square</h3>
+        <div className="regions-grid">
+          <Figure
+            caption="Demon Square, Myth War Online"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2ID34Bu5N6bXvnFgdPQZHUTqKM1iOW0uIVxAlSa"
+            fullSrc=""
+          />
+          <Figure
+            caption="Demon Square, Myth War II"
+            figureSrc="https://gnz8w62jmm.ufs.sh/f/6TomNTIXQ2IDQnyk9B0p08e7tMHlWkyhBqxZaFJbDPU3vf6G"
+            fullSrc=""
+          />
+        </div>
       </Section>
     </>
   );
