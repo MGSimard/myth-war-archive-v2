e34f6ab179736f57b54fc552cef88f78ce3cbd80
MGSimard <mgsimard.dev@gmail.com>
2025-06-11 22:34:42 -0400

    Set up ufs links for music and sound effects, I'll do backgrounds tomorrow and then the audio player itself

--- src/_components/AudioPlayer.tsx
+++ src/_components/AudioPlayer.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
+import tracks from "@/_data/assets-audio.json";
 
 export function AudioPlayer() {
   const [selectedAudio, setSelectedAudio] = useState<string | null>(null);
@@ -10,30 +11,23 @@ export function AudioPlayer() {
           <h3>Audio List</h3>
         </div>
         <div id="audio-list-content">
-          <h4>Music</h4>
-          <ul>
-            <li>
-              <button type="button" aria-label="Select Audio">
-                Audio Track 1
-              </button>
-            </li>
-          </ul>
-          <h4>Ambient</h4>
-          <ul>
-            <li>
-              <button type="button" aria-label="Select Audio">
-                Audio Track 2
-              </button>
-            </li>
-          </ul>
-          <h4>Sound Effects</h4>
-          <ul>
-            <li>
-              <button type="button" aria-label="Select Audio">
-                Audio Track 3
-              </button>
-            </li>
-          </ul>
+          {Object.entries(tracks).map(([category, tracks]) => (
+            <Fragment key={category}>
+              <h4 className="label">{category}</h4>
+              <ul>
+                {tracks.map((track) => (
+                  <li key={track.file}>
+                    <button
+                      type="button"
+                      aria-label={`Select ${track.file}`}
+                      onClick={() => setSelectedAudio(track.file)}>
+                      {track.file}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </Fragment>
+          ))}
         </div>
       </div>
       <div id="audio-right">
