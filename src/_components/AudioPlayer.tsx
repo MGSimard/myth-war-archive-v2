5ed9a9a423062277bd2bd3d218f82483c92fa9f7
MGSimard <mgsimard.dev@gmail.com>
2025-06-12 11:35:00 -0400

    Settle for an audio player container that'll span the page

--- src/_components/AudioPlayer.tsx
+++ src/_components/AudioPlayer.tsx
@@ -1,14 +1,20 @@
 import { Fragment, useState } from "react";
 import tracks from "@/_data/assets-audio.json";
 
+interface TrackTypes {
+  file: string;
+  description: string;
+  link: string;
+}
+
 export function AudioPlayer() {
-  const [selectedAudio, setSelectedAudio] = useState<string | null>(null);
+  const [selectedAudio, setSelectedAudio] = useState<TrackTypes | null>(null);
 
   return (
     <>
       <div id="audio-list">
         <div id="audio-list-header">
-          <h3>Audio List</h3>
+          <h3 className="label">Audio List</h3>
         </div>
         <div id="audio-list-content">
           {Object.entries(tracks).map(([category, tracks]) => (
@@ -17,10 +23,7 @@ export function AudioPlayer() {
               <ul>
                 {tracks.map((track) => (
                   <li key={track.file}>
-                    <button
-                      type="button"
-                      aria-label={`Select ${track.file}`}
-                      onClick={() => setSelectedAudio(track.file)}>
+                    <button type="button" aria-label={`Select ${track.file}`} onClick={() => setSelectedAudio(track)}>
                       {track.file}
                     </button>
                   </li>
@@ -31,13 +34,13 @@ export function AudioPlayer() {
         </div>
       </div>
       <div id="audio-right">
-        <ul>
+        <ul aria-atomic="true" aria-live="polite" aria-label="Current Track Information">
           <li className="label">File Name [FIELD]</li>
           <li className="label">File Length [FIELD]</li>
           <li className="label">Details [FIELD]</li>
         </ul>
+        <div id="audio-controls">Audio Controls</div>
       </div>
-      <div id="audio-controls">Audio Controls</div>
     </>
   );
 }
