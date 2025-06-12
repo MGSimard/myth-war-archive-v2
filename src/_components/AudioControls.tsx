7b1256dcfebde787785d5a9134e7d0110dd9d266
MGSimard <mgsimard.dev@gmail.com>
2025-06-12 14:19:48 -0400

    a

---
+++ src/_components/AudioControls.tsx
@@ -0,0 +1,3 @@
+export default function AudioControls() {
+  return <div id="audio-controls">AudioControls</div>;
+}
--- src/_components/AudioPlayer.tsx
+++ src/_components/AudioPlayer.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react";
 import tracks from "@/_data/assets-audio.json";
+import AudioControls from "./AudioControls";
 
 interface TrackTypes {
   file: string;
@@ -23,7 +24,11 @@ export function AudioPlayer() {
               <ul>
                 {tracks.map((track) => (
                   <li key={track.file}>
-                    <button type="button" aria-label={`Select ${track.file}`} onClick={() => setSelectedAudio(track)}>
+                    <button
+                      type="button"
+                      aria-label={`Select ${track.file}`}
+                      onClick={() => setSelectedAudio(track)}
+                      className={selectedAudio?.file === track.file ? "selected" : ""}>
                       {track.file}
                     </button>
                   </li>
@@ -34,12 +39,21 @@ export function AudioPlayer() {
         </div>
       </div>
       <div id="audio-right">
-        <ul aria-atomic="true" aria-live="polite" aria-label="Current Track Information">
-          <li className="label">File Name [FIELD]</li>
-          <li className="label">File Length [FIELD]</li>
-          <li className="label">Details [FIELD]</li>
-        </ul>
-        <div id="audio-controls">Audio Controls</div>
+        <div id="audio-info" aria-atomic="true" aria-live="polite" aria-label="Current Track Information">
+          <label htmlFor="audio-file-name">
+            <span className="label">File Name:</span>{" "}
+            <input id="audio-file-name" type="text" value={selectedAudio?.file} readOnly />
+          </label>
+          <label htmlFor="audio-length">
+            <span className="label">File Length:</span>
+            <input id="audio-length" type="text" value={selectedAudio?.length} readOnly />
+          </label>
+          <label htmlFor="audio-details">
+            <span className="label">Details:</span>
+            <textarea id="audio-details" value={selectedAudio?.description} readOnly rows={5} />
+          </label>
+        </div>
+        <AudioControls />
       </div>
     </>
   );
