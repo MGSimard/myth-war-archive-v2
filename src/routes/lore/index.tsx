30cae24387f55e4ca89621de0797d57113349384
MGSimard <mgsimard.dev@gmail.com>
2025-06-11 21:35:58 -0400

    Fix up class descs the originals are jank

--- src/routes/lore/index.tsx
+++ src/routes/lore/index.tsx
@@ -126,22 +126,22 @@ function PageLore() {
             <p>
               Paladins are great warriors who have a heart to save the weak. Their main purpose in life is to pursue
               Godliness. They spend their lives training themselves up in order to protect their lands and as a result,
-              they have advanced combating skills and possess greater stamina and power than other classes. They are
-              mainly noted for their superior crime and combat skills. Apart from the offensive side of Paladin nature,
-              they are also good at defense. One can term them as moving castles during battles. However one drawback of
-              this class is that the equipment they carry is relatively heavy because of all the heavy-based weapons and
-              armor they carry and therefore their agility is compromised. A high level paladin is able to advance to a
+              they have advanced combat skills and possess greater stamina and power than other classes. They are mainly
+              noted for their superior combat skills. Apart from the offensive side of Paladin nature, they are also
+              good at defense. One can term them as moving castles during battles. However one drawback of this class is
+              that the equipment they carry is relatively heavy because of all the heavy-based weapons and armor they
+              carry and therefore their agility is compromised. A high-level Paladin is able to advance to a
               magic-swordsman.
             </p>
             <h4>Thief</h4>
             <p>
               Thieves are known to have a wicked and nasty nature. Their outward appearance takes on the usual shape of
-              a slim body and they dress in a corset and mask mainly. Their main choice of weapons are daggers and
-              knives. Their agility level is the highest amongst all classes. Obviously, they are experts in stealing
-              money and items from their enemies as that is what they do. They gain fame and recognition from this
-              activity. Their fast attacking speeds show no mercy to their enemies and if necessary they are able to
-              evade attacks as well. Thieves can be powerful allies to team up with. A high leveled thief can be
-              advanced to become an assassinator.
+              a slim body and they dress in a corset and mask mainly. Their preferred weapons are daggers and knives.
+              Their agility level is the highest amongst all classes. Obviously, they are experts in stealing money and
+              items from their enemies as that is what they do. They gain fame and recognition from this activity. Their
+              fast attacking speeds show no mercy to their enemies and if necessary they are able to evade attacks as
+              well. Thieves can be powerful allies to team up with. A high-level Thief can advance to become an
+              Assassin.
             </p>
           </div>
           <hr />
@@ -149,31 +149,31 @@ function PageLore() {
             <h3>Centaurs</h3>
             <p>
               Like humans, centaurs have a long history and civilization. In contrast with other races, centaurs have a
-              social system whereby the female possesses a higher status than a male in the common population. They are
-              prized to be noble and beautiful. Centaurs live atop the World Tree. Since they rely on the life water
-              from the World Tree, they are able to live on over hundreds or even a thousand years. They love peace and
-              like to live in a quiet environment. When Centaurs choose to apply forces, it is mainly because they are
-              protecting their own land or preserving their set of rules. There is no battle in the world relevant to
-              justice that Centaurs do not show up. They are born with great agility and have a higher intelligence
-              level compared to humans. Greater knowledge and more magic skills make centaurs a formidable entity.
+              social system where females hold higher status than males. They are prized for being noble and beautiful.
+              Centaurs live atop the World Tree. Since they rely on the life water from the World Tree, they are able to
+              live on over hundreds or even a thousand years. They love peace and like to live in a quiet environment.
+              When Centaurs choose to use force, it is mainly because they are protecting their own land or preserving
+              their set of rules. There is no just battle in which Centaurs fail to appear. They are born with great
+              agility and have a higher intelligence level compared to humans. Greater knowledge and more magic skills
+              make Centaurs formidable.
             </p>
             <h4>Archer</h4>
             <p>
-              Strength, agility and intelligence are never enough to archers who spend their lives to pursue peace and
-              justice. They adopt range weapons, and are masters in using bows and crossbows. They are born with
-              fantastic foresight that give them a great tactical advantage even before battles start. Normally, they
-              dress lightly with light armors or silky clothes to make sure their agilities will not be affected; One
-              disadvantage of this is that their armor shielding is low and they have low hit points as well. A
-              high-leveled archer is up gradable to become a hunter.
+              Strength, agility, and intelligence are never enough for Archers, who spend their lives pursuing peace and
+              justice. They favor ranged weapons and are masters of bows and crossbows. They are born with fantastic
+              foresight that gives them a great tactical advantage even before battles start. Normally, they dress in
+              light armor or silk clothing to ensure their agility is not hindered; One disadvantage of this is that
+              their armor shielding is low and they have low hit points as well. A high-level Archer can be upgraded to
+              become a Hunter.
             </p>
             <h4>Elf</h4>
             <p>
               Elves have a lot of respect for nature and are often one with nature. This enables them to tune in to
-              their natural healing and blessing skills. Elves are very important team platers as they move fast and
+              their natural healing and blessing skills. Elves are very important team players as they move fast and
               attack fast as well as have high chances to evade attacks. Elves have great advantages when facing enemies
-              like the undead because they are able to bless teammates with the force of nature. Elves however are very
-              delicate and there is a strong need to protect them in battle. High leveled elves are up gradable to
-              become Knight-elves.
+              like the undead because they are able to bless teammates with the force of nature. Elves, however, are
+              very delicate and there is a strong need to protect them in battle. High-level Elves can be upgraded to
+              become Knight-Elves.
             </p>
           </div>
           <hr />
@@ -183,56 +183,56 @@ function PageLore() {
               Mages are a combination of the human and centaur class. Their race was cursed ever since it existed and
               was cursed again after they sold their souls to the dark lord. Their skin is a weird tinge of blue due to
               a curse by the Gods. This prevents them from ever seeing or being in sunlight. The lands they inhabit are
-              filled with nothing but darkness. This unfairness they felt lead to great hatred and a strong desire to
-              take revenge. However they were clever to forgive to the world and progress in a more positive direction.
-              By using their intelligence, they established the largest economic center in the planet and gained trust
-              by helping strong races to develop and prosper.
+              filled with nothing but darkness. This unfairness they felt led to great hatred and a strong desire to
+              take revenge. However, they were wise enough to forgive the world and progress in a more positive
+              direction. By using their intelligence, they established the largest economic center on the planet and
+              gained trust by helping strong races to develop and prosper.
             </p>
             <h4>Priest</h4>
             <p>
               Priests are capable of casting incredibly powerful spells by utilizing the powers of wind, thunder, water
               and fire. Their brand of magic is dark in opposition to the holy one. As they are deprived of sunlight,
-              they are weak at closed battle. They compensate for this weakness by having spells which can have
+              they are weak in close combat. They compensate for this weakness by having spells which can have
               unbelievable powers, having great advantages over their enemies during melee attacks. However, their
               weakest links are their low armor and low hit points. A priest is able to advance to High Priest.
             </p>
             <h4>Witch</h4>
             <p>
               Witches have the power to combine ancient charms to exert influences over substances and matter. They hold
-              the power to announce death and use this power in battle. They have similar attack features as priest such
+              the power to pronounce death and use this power in battle. They have similar attack traits to Priests such
               as low hit points, but they have a comparatively higher attack damage. A witch can be advanced to become a
-              high-witch.
+              High Witch.
             </p>
           </div>
           <hr />
           <div className="papyrus-section">
             <h3>Borgs</h3>
             <p>
-              Half human robots were developed by humans during the decades of miracles. After the ranking wars, the
-              release of the power from the magical stones caused most of the infrastructure to be destroyed. Leaving
-              the robots no power to maintain themselves. Most of them were shutdown having a long winter sleep under
-              ground. Some of them decayed and disappeared along the way and some managed to preserve themselves. When
-              the Myth War arrived, mages stumbled across them and it ignited the mages interests, since mages had been
-              trying to develop something that could overcome their physical shortcomings. "Silver Driver" - an
-              embodiment of the mages advanced intelligence was put in the robots body to make them move again. Parts of
-              this robot would release rays of light as the Silver Driver was operating. Moreover, inspired by
+              Half-human robots were developed by humans during the decades of miracles. After the ranking wars, the
+              release of the power from the magical stones caused most of the infrastructure to be destroyed, leaving
+              the robots no power to maintain themselves. Most of them were shut down, entering a long winter sleep
+              underground. Some of them decayed and disappeared along the way and some managed to preserve themselves.
+              When the Myth War arrived, mages stumbled across them and it ignited the mages' interest, since mages had
+              been trying to develop something that could overcome their physical shortcomings. "Silver Driver" - an
+              embodiment of the mages' advanced intelligence was put in the robot's body to make them move again. Parts
+              of this robot would release rays of light as the Silver Driver was operating. Moreover, inspired by
               technology the mages invented a flight propulsion system for the robots which enabled them to fly without
               using magic.
             </p>
             <h4>Tyrant</h4>
             <p>
-              Tyrants emerge from the ruins so they are more adept to battle, having a super-powerful body. They are
+              Tyrants emerge from the ruins so they are more adept at battle, having a super-powerful body. They are
               great experts in battle and have only one focus on their mind, to attack. Although they can only use slow
               and heavy weapons the damage inflicted is massive. They are good at absorbing hit points as well as
               rebounding attacks.
             </p>
             <h4>Cyborg</h4>
             <p>
-              Cyborgs are designed to protect and prolong lives. They have resistance to all spells being machine based
-              and they have a longer life span than their biological enemies. They excel in team battles due to their
-              healing contribution. They are also good at solo fights. Their primary weapons are spears. As a spear is a
-              double armed weapon, they cannot use shields. They have lower attacking rates, weak defense but can absorb
-              a lot of damage.
+              Cyborgs are designed to protect and prolong lives. They have resistance to all spells, being
+              machine-based, and they have a longer lifespan than their biological enemies. They excel in team battles
+              due to their healing contribution. They are also good at solo fights. Their primary weapons are spears. As
+              a spear is a two-handed weapon, they cannot use shields. They have slower attack rates, weak defense, but
+              can absorb a lot of damage.
             </p>
           </div>
         </div>
