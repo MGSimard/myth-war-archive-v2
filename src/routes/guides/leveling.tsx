a24ded7a4a1d29e858adb4742b2a02e2db9a3cea
MGSimard <mgsimard.dev@gmail.com>
2025-06-12 22:57:02 -0400

    Base

--- src/routes/guides/leveling.tsx
+++ src/routes/guides/leveling.tsx
@@ -9,15 +9,75 @@ export const Route = createFileRoute("/guides/leveling")({
 function PageLore() {
   return (
     <>
-      <Header title="Leveling" subtitle="Chronicles of Creation and Cataclysm" />
+      <Header title="Leveling" subtitle="Your Guide to Power and Progression" />
       <Section title="Level 1-6">
         <div className="papyrus">
-          <div className="papyrus-section">Jello</div>
+          <div className="papyrus-section">
+            <h3>Tutorial</h3>
+          </div>
         </div>
       </Section>
-      <Section title="Level 6-21 (New Horizon & Best Coagent">
+      <Section title="Level 6-21">
         <div className="papyrus">
-          <div className="papyrus-section">Jello</div>
+          <div className="papyrus-section">
+            <h3>New Horizon & Best Coagent</h3>
+          </div>
+        </div>
+      </Section>
+      <Section title="Level 6-21-30">
+        <div className="papyrus">
+          <div className="papyrus-section">
+            <h3>Stonesmith</h3>
+          </div>
+        </div>
+      </Section>
+      <Section title="Level 30-50">
+        <div className="papyrus">
+          <div className="papyrus-section">
+            <h3>Howling Beast</h3>
+          </div>
+        </div>
+      </Section>
+      <Section title="Level 30+">
+        <div className="papyrus">
+          <div className="papyrus-section">
+            <h3>Drow & Wednesday Event</h3>
+          </div>
+        </div>
+      </Section>
+      <Section title="Level 50-60">
+        <div className="papyrus">
+          <div className="papyrus-section">
+            <h3>Woodlingor Defense</h3>
+          </div>
+        </div>
+      </Section>
+      <Section title="Level 60-70">
+        <div className="papyrus">
+          <div className="papyrus-section">
+            <h3>Blython Defense</h3>
+          </div>
+        </div>
+      </Section>
+      <Section title="Level 60-80">
+        <div className="papyrus">
+          <div className="papyrus-section">
+            <h3>Bloodreaper</h3>
+          </div>
+        </div>
+      </Section>
+      <Section title="Level 70-90">
+        <div className="papyrus">
+          <div className="papyrus-section">
+            <h3>Seal</h3>
+          </div>
+        </div>
+      </Section>
+      <Section title="Level 90+">
+        <div className="papyrus">
+          <div className="papyrus-section">
+            <h3>Papa Drow & Worn Messenger</h3>
+          </div>
         </div>
       </Section>
     </>
