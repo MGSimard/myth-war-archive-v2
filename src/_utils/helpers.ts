7365addbc9b2f3f3ef14ad32b3dbab17f3dee7c2
MGSimard <mgsimard.dev@gmail.com>
2025-06-15 17:10:30 -0400

    sync rounding

--- src/_utils/helpers.ts
+++ src/_utils/helpers.ts
@@ -1,7 +1,7 @@
 // miliseconds to MM:SS rounded
 
 export function formatMmSs(milliseconds: number) {
-  const totalSeconds = Math.round(milliseconds / 1000);
+  const totalSeconds = Math.floor(milliseconds / 1000);
   const minutes = Math.floor((totalSeconds % 3600) / 60);
   const remainingSeconds = totalSeconds % 60;
   return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
