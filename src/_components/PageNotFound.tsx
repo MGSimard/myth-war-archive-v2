6186c44475187cc87d50b0d1e034b42118a316cc
MGSimard <mgsimard.dev@gmail.com>
2025-06-16 17:35:42 -0400

    NotFound system notice

--- src/_components/PageNotFound.tsx
+++ src/_components/PageNotFound.tsx
@@ -1,12 +1,15 @@
-import { Link } from "@tanstack/react-router";
-
 export function PageNotFound() {
   return (
-    <>
-      <h1>NOT FOUND</h1>
-      <Link to="/">
-        <span>RETURN HOME</span>
-      </Link>
-    </>
+    <section id="system-notice">
+      <div className="section-header">
+        <h1 className="heading">System Notice [404]</h1>
+      </div>
+      <div className="section-content">
+        <img src="/assets/404.webp" alt="" aria-hidden="true" />
+        <div id="system-message">
+          <h2>The page you are looking for does not exist.</h2>
+        </div>
+      </div>
+    </section>
   );
 }
