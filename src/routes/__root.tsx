940dd10b4879d2bc6123d9401395d56395aeb1bc
MGSimard <mgsimard.dev@gmail.com>
2025-09-22 18:09:15 -0400

    lang

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,7 +102,7 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <HeadContent />
       </head>
