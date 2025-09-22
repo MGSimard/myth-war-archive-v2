fcc6a6faa620a03614293705f8a1cf7934bf352a
MGSimard <mgsimard.dev@gmail.com>
2025-09-22 17:00:38 -0400

    Actually works out of the box potentially? Fix up base_url, gonna install tanstack devtools alpha

--- src/_utils/consts.ts
+++ src/_utils/consts.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE = "Myth War Archive";
 export const SITE_DESCRIPTION = "Preserving the world, art, and history of Myth War.";
 export const SITE_AUTHOR = "MGSimard";
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://mwarchive.pages.dev";
